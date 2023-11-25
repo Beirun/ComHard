819913f5222972dd29c9e7bf2ccf3c433c62fd39
Beirun <orillobernardjay@gmail.com>
2023-11-25 11:22:51 +0800

    initial commit

---
+++ src/main/java/Buttons.java
@@ -0,0 +1,102 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Buttons extends JPanel {
+    final private String Label;
+    private int width = 100;
+    private int height = 50;
+    private int arc = 45;
+    private int buttonX = 0;
+    private int buttonY = 0;
+    private int fontSize = 13;
+    private Color colorSelect = new Color(117,210,104);
+    boolean mousePressed = false, mouseReleased = false, mouseEntered = false;
+    public Buttons(String Label){
+        this.Label = Label;
+        this.setPreferredSize(new Dimension(width,height));
+        this.setBackground(null);
+        this.addMouseListener(new ButtonChanger());
+    }
+    public void setColor(Color color){
+        colorSelect = color;
+    }
+    public void setPosition(int buttonX, int buttonY){
+        this.buttonX = buttonX;
+        this.buttonY = buttonY;
+    }
+    public void setDimension(int width, int height){
+        this.width = width;
+        this.height = height;
+        this.setPreferredSize(new Dimension(width, height));
+    }
+    public void setBorderRadius(int arc){
+        this.arc = arc;
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Draw(g);
+
+    }
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public void Draw(Graphics g){
+        Color colorNormal = new Color(colorSelect.getRed(), colorSelect.getGreen(), colorSelect.getBlue());
+        Color colorPressed = new Color(colorSelect.getRed(), colorSelect.getGreen(), colorSelect.getBlue(), 125);
+        Color colorEntered = new Color(colorSelect.getRed(), colorSelect.getGreen(), colorSelect.getBlue(), 175);
+
+        if(mouseEntered || mouseReleased) g.setColor(colorEntered);
+        else if(mousePressed) g.setColor(colorPressed);
+        else g.setColor(colorNormal);
+        Rectangle(g);
+    }
+
+    public void Rectangle(Graphics g){
+        //g.fillRoundRect(112,0,width-45,height,12,12);
+        g.fillRoundRect(buttonX,buttonY,width,height,arc,arc);
+        g.setColor(new Color(38,38,38));
+        g.setFont( new Font("",Font.BOLD, fontSize));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        //
+        g.drawString(Label,(width - metrics.stringWidth(Label))/2,height/2 + g.getFont().getSize()/2);
+
+    }
+
+    class ButtonChanger extends MouseAdapter{
+
+        public ButtonChanger( ){}
+        public void mousePressed(MouseEvent e) {
+            mousePressed = true;
+            mouseEntered = false;
+            mouseReleased = false;
+            repaint();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            mousePressed = false;
+            mouseEntered = false;
+            mouseReleased = true;
+            repaint();
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            mousePressed = false;
+            mouseEntered = true;
+            mouseReleased = false;
+            repaint();
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            mousePressed = false;
+            mouseEntered = false;
+            mouseReleased = false;
+            repaint();
+        }
+    }
+}
--- src/main/java/ComHard.java
+++ src/main/java/ComHard.java
@@ -1,6 +1,7 @@
-public class ComHard{
+public class ComHard {
    static final int WIDTH = 1250, LENGTH = 700;
-   public static void main(String[] a){
+
+   public static void main(String[] a) {
       new ComHardFrame();
    }
 }
